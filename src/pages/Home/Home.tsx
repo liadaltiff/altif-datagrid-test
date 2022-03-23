507d518127a4ccb0fd0027142246ea2f9c63010c
liadaltiff <liadaltif@gmail.com>
2022-03-23 13:15:47 +0200

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,21 @@
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
+
+export default function ToolbarGrid() {
+  const { data } = useDemoData({
+    dataSet: "Commodity",
+    rowLength: 100,
+    maxColumns: 6,
+  });
+
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        {...data}
+        components={{
+          Toolbar: GridToolbar,
+        }}
+      />
+    </div>
+  );
+}
